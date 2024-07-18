e15b491323f26739d8b9b4ed1c73db6be29630c6
Void <abac987.ab@gmail.com>
2024-07-18 17:52:18 +0700

    Index test

--- functions.php
+++ functions.php
@@ -1,206 +1,224 @@
 <?php declare(strict_types=1);
 
 
-namespace Tracker {
+namespace Tracker;
 
-    use FilesystemIterator;
-    use RecursiveDirectoryIterator;
-    use RecursiveIteratorIterator;
-    use RuntimeException;
-    use SplFileInfo;
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RuntimeException;
+use SplFileInfo;
 
-    function writeFile(string $filename, string $content, int $flag = 0): bool
-    {
-        return file_put_contents($filename, $content, $flag) !== false;
-    }
+require_once __DIR__ . "/constants.php";
+require_once __DIR__ . "/src/Tracker/EnvManager.php";
 
-    function isFileAvailable(string $filename): bool
-    {
-        return file_exists($filename) && is_writable($filename);
-    }
+/**
+ * @param mixed ...$messages
+ * @return void
+ */
+function alert(...$messages): void
+{
+    $isWeb = php_sapi_name() !== 'cli';
 
-    function log(string $message): void
-    {
-        writeFile(INSTALLER_LOG_FILE, $message . PHP_EOL, FILE_APPEND);
-    }
-
-    function divider(string $message = "", bool $postEmpty = false): void
-    {
-        $preDividerLength = $postEmpty ? 6 : 12;
-        $postDividerLength = 130;
-        if ($message) {
-            $message = " {$message} ";
-            $postDividerLength -= (strlen($message) + $preDividerLength);
+    if (count($messages) > 1) {
+        foreach ($messages as $message) {
+            alert($message);
         }
-        echo str_repeat("-", $preDividerLength), $message;
-        if (!$postEmpty) {
-            echo str_repeat("-", $postDividerLength);
-        }
-        echo PHP_EOL;
-        log($message);
-    }
-
-    function line(): void
-    {
-        divider(str_repeat("*", 100));
-    }
-
-    function exitWithError(string $message): void
-    {
-        echo PHP_EOL, PHP_EOL, PHP_EOL, $message, PHP_EOL;
-        log($message);
-
-        divider(str_repeat("*", 100));
-        exit(1);
-    }
-
-    function removeDirectory(string $dir): void
-    {
-        if (is_dir($dir)) {
-            $iterator = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
-            $files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST);
-
-            /** @var SplFileInfo $file */
-            foreach ($files as $file) {
-                if ($file->isDir()) {
-                    rmdir($file->getRealPath());
-                } else {
-                    unlink($file->getRealPath());
-                }
-            }
-            rmdir($dir);
-            divider("Removed directory: {$dir}");
+    } else {
+        if ($isWeb) {
+            echo '<pre>', print_r($messages[0], true), '</pre>', PHP_EOL;
         } else {
-            divider("Directory not found: {$dir}", true);
+            echo print_r($messages[0], true), PHP_EOL;
         }
     }
+}
 
-    function removeFile(string $file): void
-    {
-        if (file_exists($file)) {
-            unlink($file);
-            divider("Removed file: {$file}");
-        } else {
-            divider("File not found: {$file}", true);
-        }
+function writeFile(string $filename, string $content, int $flag = 0): bool
+{
+    return file_put_contents($filename, $content, $flag) !== false;
+}
+
+function isFileAvailable(string $filename): bool
+{
+    return file_exists($filename) && is_writable($filename);
+}
+
+function log(string $message): void
+{
+    writeFile(INSTALLER_LOG_FILE, $message . PHP_EOL, FILE_APPEND);
+}
+
+function divider(string $message = "", bool $postEmpty = false): void
+{
+    $preDividerLength = $postEmpty ? 6 : 12;
+    $postDividerLength = 130;
+    if ($message) {
+        $message = " {$message} ";
+        $postDividerLength -= (strlen($message) + $preDividerLength);
     }
-
-    function configureDevMode(): void
-    {
-        $configFile = CONFIG_FLAG_FILE;
-        $isDev = getenv('COMPOSER_DEV_MODE');
-
-        divider("Configuring dev mode");
-        $configContent = $isDev ? "<?php const DEV_MODE = true;" : "<?php const DEV_MODE = false;";
-        if (!writeFile($configFile, $configContent)) {
-            exitWithError("Failed to write to configuration file: {$configFile}");
-        } else {
-            $message = $isDev ? "Development mode config file created: " : "Production mode config file set: ";
-            $message .= $configFile;
-            divider($message, true);
-        }
+    echo str_repeat("-", $preDividerLength), $message;
+    if (!$postEmpty) {
+        echo str_repeat("-", $postDividerLength);
     }
+    echo PHP_EOL;
+    log($message);
+}
 
-    function createDirectories(): void
-    {
-        foreach (DIRECTORIES as $dir) {
-            divider("Checking directory: {$dir}", true);
-            if (!file_exists($dir)) {
-                if (!mkdir($dir, 0755, true)) {
-                    divider("Failed to create directory: {$dir}", true);
-                    continue;
-                }
-                divider("Created directory: {$dir}", true);
+function line(): void
+{
+    divider(str_repeat("*", 100));
+}
+
+function exitWithError(string $message): void
+{
+    echo PHP_EOL, PHP_EOL, PHP_EOL, $message, PHP_EOL;
+    log($message);
+
+    divider(str_repeat("*", 100));
+    exit(1);
+}
+
+function removeDirectory(string $dir): void
+{
+    if (is_dir($dir)) {
+        $iterator = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST);
+
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            if ($file->isDir()) {
+                rmdir($file->getRealPath());
             } else {
-                divider("Directory already exists: {$dir}", true);
+                unlink($file->getRealPath());
             }
         }
+        rmdir($dir);
+        divider("Removed directory: {$dir}");
+    } else {
+        divider("Directory not found: {$dir}", true);
+    }
+}
+
+function removeFile(string $file): void
+{
+    if (file_exists($file)) {
+        unlink($file);
+        divider("Removed file: {$file}");
+    } else {
+        divider("File not found: {$file}", true);
+    }
+}
+
+function configureDevMode(): void
+{
+    $configFile = CONFIG_FLAG_FILE;
+    $isDev = getenv('COMPOSER_DEV_MODE');
+
+    divider("Configuring dev mode");
+    $configContent = $isDev ? "<?php const DEV_MODE = true;" : "<?php const DEV_MODE = false;";
+    if (!writeFile($configFile, $configContent)) {
+        exitWithError("Failed to write to configuration file: {$configFile}");
+    } else {
+        $message = $isDev ? "Development mode config file created: " : "Production mode config file set: ";
+        $message .= $configFile;
+        divider($message, true);
+    }
+}
+
+function createDirectories(): void
+{
+    foreach (DIRECTORIES as $dir) {
+        divider("Checking directory: {$dir}", true);
+        if (!file_exists($dir)) {
+            if (!mkdir($dir, 0755, true)) {
+                divider("Failed to create directory: {$dir}", true);
+                continue;
+            }
+            divider("Created directory: {$dir}", true);
+        } else {
+            divider("Directory already exists: {$dir}", true);
+        }
+    }
+}
+
+/**
+ * @param array<string, mixed> $arguments
+ * @return void
+ */
+function setRemoteConfig(array $arguments = []): void
+{
+    if (empty($arguments)) {
+        divider("Empty remote configuration, use default values");
+        return;
     }
 
-    /**
-     * @param array<string, string> $arguments
-     * @return void
-     */
-    function setRemoteConfig(array $arguments): void
-    {
-        if (empty($arguments)) {
-            divider("Empty remote configuration, use default values");
-            return;
-        }
+    try {
+        $envFile = ENV_FILE;
+        EnvManager::setup(ENV_FILE, $arguments);
+        divider("{$envFile} file updated successfully.");
+    } catch (RuntimeException $e) {
+        exitWithError($e->getMessage());
+    }
+}
+
+/**
+ * @return void
+ */
+function configureRemote(): void
+{
+    if (isFileAvailable(ENV_FILE)) {
 
         try {
             $envFile = ENV_FILE;
-            EnvManager::setup(ENV_FILE, $arguments);
+            EnvManager::setup(ENV_FILE, [
+                CONFIG_SECTION_REMOTE_HOST => 'localhost',
+                CONFIG_SECTION_REMOTE_PORT => 1088,
+            ]);
             divider("{$envFile} file updated successfully.");
         } catch (RuntimeException $e) {
             exitWithError($e->getMessage());
         }
     }
+}
 
-    /**
-     * @return void
-     */
-    function configureRemote(): void
-    {
-        if (isFileAvailable(ENV_FILE)) {
+function configureEnvironment(): void
+{
+    configureDevMode();
+    configureRemote();
+}
 
-            try {
-                $envFile = ENV_FILE;
-                EnvManager::setup(ENV_FILE, [
-                    CONFIG_SECTION_REMOTE_HOST => 'localhost',
-                    CONFIG_SECTION_REMOTE_PORT => 1088,
-                ]);
-                divider("{$envFile} file updated successfully.");
-            } catch (RuntimeException $e) {
-                exitWithError($e->getMessage());
-            }
-        }
-    }
+function install(): void
+{
+    createDirectories();
+    configureEnvironment();
+}
 
-    function configureEnvironment(): void
-    {
-        configureDevMode();
-        configureRemote();
-    }
+function reInstall(): void
+{
+    removeDirectory(RUNTIME_PATH);
+    removeFile(CONFIG_FLAG_FILE);
+    removeFile(ENV_FILE);
+}
 
-    function install(): void
-    {
-        createDirectories();
-        configureEnvironment();
-    }
+/**
+ * @return array{array{install-dev: bool, install-prod: bool}, array{Tracker_REMOTE_HOST: list<mixed>|string|false, Tracker_REMOTE_PORT: list<mixed>|string|false}}
+ */
+function parseArguments(): array
+{
+    $options = getopt("h:p", [
+        "host:",
+        "port:",
+    ]);
 
-    function reInstall(): void
-    {
-        removeDirectory(RUNTIME_PATH);
-        removeFile(CONFIG_FLAG_FILE);
-        removeFile(ENV_FILE);
-    }
+    $actions = [
+        'install-dev' => isset($options['install-dev']),
+        'install-prod' => isset($options['install-prod']),
+    ];
 
-    /**
-     * @return array{array{install-dev: bool, install-prod: bool}, array{Tracker_REMOTE_HOST: list<mixed>|string|false, Tracker_REMOTE_PORT: list<mixed>|string|false}}
-     */
-    function parseArguments(): array
-    {
-        $options = getopt("s:h:u:p:d", [
-            "driver",
-            "host:",
-            "user:",
-            "pass:",
-            "database:",
-            "install-dev",
-            "install-prod"
-        ]);
-        $actions = [
-            'install-dev' => isset($options['install-dev']),
-            'install-prod' => isset($options['install-prod']),
-        ];
+    $remoteConfig = [
+        CONFIG_SECTION_REMOTE_HOST => $options['h'] ?? $options['host'] ?? 'localhost',
+        CONFIG_SECTION_REMOTE_PORT => $options['p'] ?? $options['port'] ?? '1088',
+    ];
 
-        $remoteConfig = [
-            CONFIG_SECTION_REMOTE_HOST => $options['h'] ?? $options['host'] ?? 'localhost',
-            CONFIG_SECTION_REMOTE_PORT => $options['p'] ?? $options['port'] ?? '1088',
-        ];
-
-        return [$actions, $remoteConfig];
-    }
-
+    return [$actions, $remoteConfig];
 }
--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php declare(strict_types=1);
 
 
-
 $autoload = realpath(__DIR__ . '/../vendor/autoload.php');
 
-if(file_exists($autoload)) {
+if (file_exists($autoload)) {
     require_once $autoload;
 
 
-
+    var_dump(class_exists("\\Tracker\\EnvManager"));
 
 }
