9be6c18b86212822d3fee4eab3a4cae4ef494e0e
Void <abac987.ab@gmail.com>
2024-07-18 18:05:28 +0700

    Add logger

--- public/index.php
+++ public/index.php
@@ -1,12 +1,62 @@
 <?php declare(strict_types=1);
 
 
+use Tracker\Log\Logger;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
 $autoload = realpath(__DIR__ . '/../vendor/autoload.php');
 
 if (file_exists($autoload)) {
     require_once $autoload;
 
+    if (!defined("DEV_MODE")) {
+        define("DEV_MODE", false);
+    }
 
-    var_dump(class_exists("\\Tracker\\EnvManager"));
+    if (DEV_MODE) {
+        $whoops = new Run;
+        $whoops->pushHandler(new PrettyPageHandler());
+        $whoops->register();
+    } else {
+        Kint::$enabled_mode = false;
+    }
+
+    set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+        $logger = Logger::getLogger();
+
+        $errorTypes = [
+            E_ERROR => 'Error',
+            E_WARNING => 'Warning',
+            E_PARSE => 'Parse Error',
+            E_NOTICE => 'Notice',
+            E_CORE_ERROR => 'Core Error',
+            E_CORE_WARNING => 'Core Warning',
+            E_COMPILE_ERROR => 'Compile Error',
+            E_COMPILE_WARNING => 'Compile Warning',
+            E_USER_ERROR => 'User Error',
+            E_USER_WARNING => 'User Warning',
+            E_USER_NOTICE => 'User Notice',
+            E_STRICT => 'Runtime Notice',
+            E_RECOVERABLE_ERROR => 'Catchable Fatal Error',
+            E_DEPRECATED => 'Deprecated',
+            E_USER_DEPRECATED => 'User Deprecated',
+        ];
+
+        $type = $errorTypes[$errno] ?? 'Unknown Error';
+
+        $message = sprintf('%s: %s in %s on line %d', $type, $errstr, $errfile, $errline);
+
+        if ($errno === E_ERROR || $errno === E_CORE_ERROR || $errno === E_COMPILE_ERROR || $errno === E_USER_ERROR) {
+            $logger->critical($message);
+        } else {
+            $logger->warning($message);
+        }
+
+        return false;
+    });
+
+
+    echo "Hello World!";
 
 }
---
+++ src/Tracker/Log/Logger.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Tracker\Log;
+
+use Monolog\Logger as MonologLogger;
+use Monolog\Handler\StreamHandler;
+use const Tracker\CRITICAL_LOG_FILE;
+
+class Logger
+{
+    private static MonologLogger $logger;
+
+    public static function getLogger(): MonologLogger
+    {
+        if (self::$logger === null) {
+            self::$logger = new MonologLogger('tracker');
+            self::$logger->pushHandler(new StreamHandler(CRITICAL_LOG_FILE, MonologLogger::CRITICAL));
+        }
+
+        return self::$logger;
+    }
+}
